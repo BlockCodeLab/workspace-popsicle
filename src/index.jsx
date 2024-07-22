efe4434cb3d0bfab9756ab08417bd8c45b3b868a
Yeqin Gong <gon@gyeq.in>
2024-07-22 13:47:26 +0800

    update name

--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,18 @@ export default {
   disabled: true,
   sortIndex: 4,
   image: featureImage,
-  name: 'Popsicle',
+  name: 'Popsicle Box',
   description: 'The equipment can play with the real world.',
   blocksRequired: true,
 
   // l10n
   translations: {
     en: {
-      name: 'Popsicle',
+      name: 'Popsicle Box',
       description: 'The equipment can play with the real world.',
     },
     'zh-Hans': {
-      name: 'Popsicle',
+      name: 'Popsicle Box',
       description: '手里的小设备，玩转真实的大世界。',
     },
   },
